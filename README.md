11809446312d5bb31d28ec4010753b13329823e5
Christophe Deneuve <33314621+christopheOclock@users.noreply.github.com>
2021-09-01 17:00:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portfolio : intégration et dynamisation
+
+Les consignes sont assez sommaires :
+
+- Faire cette intégration, basée sur une seule capture écran (ouvrez l'image sous VScode pour la voir à 100%).
+- La plupart des connaissances vues en saison HTML/CSS vous seront utiles.
+- Commencez par la structure HTML/PHP, puis les règles CSS.
+- Importez le fichier data.php dans votre page, et utilisez les variables qui y sont déclarées pour remplir dynamiquement l'intérieur du tableau de compétences (partie Bio) et la liste d'articles (partie Work)
+- Ne traitez pas forcément tout d'un coup, dégrossissez les éléments principaux, puis faites des allers-retours entre HTML et CSS si besoin pour peaufiner les détails.
+- Pensez à utiliser les images libres de droit utilisées dans le challenge "Page perso" (ou [dummyimage](https://dummyimage.com/)).
+
+Bon courage :muscle: !
+
+![](resultat.png)
---
+++ data.php
@@ -0,0 +1,37 @@
+<?php
+
+// Données à propos de mes compétences en langages web
+$languageSkills = [
+  'HTML' => 4,
+  'CSS' => 3,
+  'PHP' => 5
+];
+
+// Données à propos de mes compétences en systèmes d'exploitation
+$environmentSkills = [
+  'Linux' => 4,
+  'MacOS' => 5,
+  'Windows' => 3
+];
+
+// Données à propos des travaux que j'ai effectués
+$workPosts = [
+  1 => [
+    "title" => "Article 1",
+    "picture" => "article-1.jpg",
+    "summary" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "link" => "article-1.php"
+  ],
+  2 => [
+    "title" => "Article 2",
+    "picture" => "article-2.jpg",
+    "summary" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor",
+    "link" => "article-2.php"
+  ],
+  3 => [
+    "title" => "Article 3",
+    "picture" => "article-3.jpg",
+    "summary" => "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "link" => "article-3.php"
+  ]
+];
